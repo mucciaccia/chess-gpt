2967abe502cd714488d41d9b52eb387fcb55986a
Sérgio Mucciaccia <sergio.mucciaccia@ufes.br>
2023-12-11 16:22:30 -0300

    Add new animation to promove pawns when they reach the end

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ square_size = 60
 
 pieces = {}
 pieces[b'0'] = None
+pieces[b'x'] = pygame.image.load('./images/cancel.svg')
 pieces[b'k'] = pygame.image.load('./images/black_king.svg')
 pieces[b'q'] = pygame.image.load('./images/black_queen.svg')
 pieces[b'r'] = pygame.image.load('./images/black_rook.svg')
@@ -41,6 +42,9 @@ board = np.matrix([
 
 piece_held = b'0'
 
+promotion = None
+promotion_pieces = [b'q', b'r', b'b', b'n']
+
 run = True
 
 while run:
@@ -54,10 +58,25 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            piece_held = board[mouse_row, mouse_column]
-            board[mouse_row, mouse_column] = b'0'
+            if promotion is None:
+                piece_held = board[mouse_row, mouse_column]
+                piece_held_last_row = mouse_row
+                piece_held_last_column = mouse_column
+                board[mouse_row, mouse_column] = b'0'
+            elif promotion == mouse_column and mouse_row < 4:
+                board[0, promotion] = promotion_pieces[mouse_row]
+                piece_held = b'0'
+                promotion = None
+            else:
+                promotion = None
+                board[piece_held_last_row, piece_held_last_column] = promoted_piece
+
         if event.type == pygame.MOUSEBUTTONUP:
-            if piece_held != b'0':
+            if piece_held == b'p' and mouse_row == 0:
+                promotion = mouse_column
+                promoted_piece = piece_held
+                piece_held = b'0'
+            elif piece_held != b'0':
                 board[mouse_row, mouse_column] = piece_held
                 piece_held = b'0'
 
@@ -70,6 +89,15 @@ while run:
 
     if piece_held != b'0':
         win.blit(pieces[piece_held], (mouse_x - 30, mouse_y - 30))
+
+    if promotion is not None:
+        color = (255,255,255)
+        pygame.draw.rect(win, color, pygame.Rect(60*promotion, 0, 60, 300))
+        win.blit(pieces[b'q'], (60 * promotion, 0))
+        win.blit(pieces[b'r'], (60 * promotion, 60))
+        win.blit(pieces[b'b'], (60 * promotion, 120))
+        win.blit(pieces[b'n'], (60 * promotion, 180))
+        win.blit(pieces[b'x'], (60 * promotion, 240))
 
     pygame.display.update()
 
